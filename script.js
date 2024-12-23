4de1bdd3f7166436f90b382a5542a1226cdf8f0d
Jeffy Pun <167040139+Jeffy46@users.noreply.github.com>
2024-12-22 20:09:30 -0500

    hover logic fixed

--- script.js
+++ script.js
@@ -6,9 +6,11 @@ let refresh = (dimension) =>{
     console.log(sideLength)
     for(let i = 0; i < dimension*dimension; i++){
         let temp = div.cloneNode();
+        temp.addEventListener("mouseover", () =>{
+            temp.style.backgroundColor="black";
+        }) 
         temp.style.width=sideLength + "px";
         temp.style.height=sideLength + "px";
-        temp.classList.toggle("hoverable");
         container.append(temp);
     }
 }
