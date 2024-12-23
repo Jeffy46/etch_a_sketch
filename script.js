1e436dbcd3bc247f2bbbcf4de3a799dab8d0b2b9
Jeffy Pun <167040139+Jeffy46@users.noreply.github.com>
2024-12-22 21:15:19 -0500

    complete

--- script.js
+++ script.js
@@ -1,16 +1,35 @@
 let container = document.querySelector("#container");
 const div = document.createElement("div");
-container.appendChild(div);
+let opacity=0.3;
+
+
 let refresh = (dimension) =>{
     let sideLength=800/dimension;
-    console.log(sideLength)
     for(let i = 0; i < dimension*dimension; i++){
         let temp = div.cloneNode();
         temp.addEventListener("mouseover", () =>{
-            temp.style.backgroundColor="black";
+            temp.style.backgroundColor=getRandomColor();
+            opacity+=0.05;
+            temp.style.opacity=opacity;
+            if(Math.floor(opacity)==1){
+                opacity=0.3;
+            }
+            
         }) 
         temp.style.width=sideLength + "px";
         temp.style.height=sideLength + "px";
         container.append(temp);
     }
+}
+let newDimensions=(dimension)=>{
+    container.innerHTML="";
+    refresh(dimension);
+}
+let getRandomColor=()=> {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
 }
